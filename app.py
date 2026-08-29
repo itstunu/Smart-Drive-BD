d0f5dfd8372857342c2823438b000603c802c098
Tasnima Tahrim Turna <tasnimaturna23@gmail.com>
2026-08-29 12:23:20 +0600

    Modify violation location
    Updated violation locations

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ class DriveDB:
         vtypes = ["Red Light Crossing", "Speeding", "Wrong Lane", "Illegal Parking",
                   "Helmet Violation", "Seat Belt Violation", "Wrong Direction", "Signal Violation"]
         locs = ["Gulshan Circle 1, Dhaka", "Mirpur 10, Dhaka", "Motijheel C/A, Dhaka",
-                "Uttara Sector 7, Dhaka", "GEC Circle, Chattogram", "Zindabazar, Sylhet",
+                "Uttara Sector 8, Dhaka", "GEC Circle, Chattogram", "Zindabazar, Sylhet",
                 "Shahbagh, Dhaka", "Farmgate, Dhaka"]
         officers = ["Insp. M. Karim", "Sgt. F. Rahman", "Insp. S. Hossain", "Sgt. A. Islam"]
         
